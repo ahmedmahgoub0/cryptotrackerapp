74adbcd3001f671b576e5012c51be802b2c161ef
Your GitHub Username <aed7862@gmail.com>
2024-10-21 21:49:13 +0300

    test

--- app/src/main/java/com/acoding/cryptotrackerapp/MainActivity.kt
+++ app/src/main/java/com/acoding/cryptotrackerapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CryptoTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Assndroid",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
