7f5161b6d3429c750c92afd41c6995be0ed4bed6
ahmedmahgoub0 <aed7862@gmail.com>
2024-10-25 04:29:06 +0300

    edit zone id to system default

--- app/src/main/java/com/acoding/cryptotrackerapp/cryptotracker/data/mappers/CoinMapper.kt
+++ app/src/main/java/com/acoding/cryptotrackerapp/cryptotracker/data/mappers/CoinMapper.kt
@@ -24,6 +24,6 @@ fun CoinPriceDto.toCoinPrice(): CoinPrice {
         priceUsd = priceUsd,
         time = Instant
             .ofEpochMilli(time)
-            .atZone(ZoneId.of("UTC"))
+            .atZone(ZoneId.systemDefault())
     )
 }
