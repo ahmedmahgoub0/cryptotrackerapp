469a2e4a6a2f5d33187c822e0e8a3888d29d5903
ahmedmahgoub0 <aed7862@gmail.com>
2024-10-31 16:33:40 +0300

    - added shimmer layout - correct imports for exceptions

--- app/src/main/java/com/acoding/cryptotrackerapp/core/data/networking/safeCall.kt
+++ app/src/main/java/com/acoding/cryptotrackerapp/core/data/networking/safeCall.kt
@@ -3,10 +3,10 @@ package com.acoding.cryptotrackerapp.core.data.networking
 import com.acoding.cryptotrackerapp.core.domain.util.NetworkError
 import com.acoding.cryptotrackerapp.core.domain.util.Result
 import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import io.ktor.utils.io.errors.IOException
 import kotlinx.coroutines.ensureActive
 import kotlinx.serialization.SerializationException
-import java.io.IOException
-import java.nio.channels.UnresolvedAddressException
 import kotlin.coroutines.coroutineContext
 
 suspend inline fun <reified T> safeCall(
@@ -14,12 +14,12 @@ suspend inline fun <reified T> safeCall(
 ): Result<T, NetworkError> {
     val response = try {
         execute()
+    } catch (e: IOException) {
+        return Result.Error(NetworkError.NO_INTERNET)
     } catch (e: UnresolvedAddressException) {
         return Result.Error(NetworkError.NO_INTERNET)
     } catch (e: SerializationException) {
         return Result.Error(NetworkError.SERIALIZATION)
-    } catch (e: IOException) {
-        return Result.Error(NetworkError.REQUEST_TIMEOUT)
     } catch (e: Exception) {
         coroutineContext.ensureActive()
         return Result.Error(NetworkError.UNKNOWN)
